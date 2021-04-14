0aadaeebb4e93eb28bb62b4c8dbadcd27831576d
Patrick Joel <joekenpat@gmail.com>
2021-04-14 21:51:06 +0100

    added mock data file

---
+++ MockData.js
@@ -0,0 +1,21 @@
+/**
+ * Array of dummy data to be use
+ * in place of a database
+ */
+const dummyData = [
+  {
+    content: [
+      "lorem ips loremCurrently there is no native support",
+      "for private properties in JavaScript ",
+      "it is possible to mimic private properties but",
+    ],
+  },
+  {
+    content: [
+      "lorem ips loremCurrently there is no native support",
+      "for private properties in JavaScript ",
+      "it is possible to mimic private properties but",
+    ],
+  },
+];
+
