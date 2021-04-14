0e2dfdf5dfb7b70eb5d050d3f877604d3ec0986b
Patrick Joel <joekenpat@gmail.com>
2021-04-14 21:59:35 +0100

    added route for content id checking

--- index.js
+++ index.js
@@ -5,25 +5,48 @@ const app = express();
 const port = 3000;
 const db = new MockData();
 
-
 app.get("/section/:sectionId", (req, res) => {
-    const sectionId = req.params.sectionId;
-    if (!db._validateSectionId(sectionId)) {
-      const notFound = {
-        status: "error",
-        message: "Invalid Section Id!",
-      };
-      res.status(404).json(notFound);
-    } else {
-      const data = {
-        status: "success",
-        message:`Section: ${sectionId} found!`,
-        data: db.allContentBySectionId(sectionId),
-      };
-      res.status(200).json(data);
-    }
-  });
+  const sectionId = req.params.sectionId;
+  if (!db._validateSectionId(sectionId)) {
+    const notFound = {
+      status: "error",
+      message: "Invalid Section Id!",
+    };
+    res.status(404).json(notFound);
+  } else {
+    const data = {
+      status: "success",
+      message: `Section: ${sectionId} found!`,
+      data: db.allContentBySectionId(sectionId),
+    };
+    res.status(200).json(data);
+  }
+});
 
+app.get("/section/:sectionId/content/:contentId", (req, res) => {
+  const sectionId = req.params.sectionId;
+  const contentId = req.params.contentId;
+  if (!db._validateSectionId(sectionId)) {
+    const notFound = {
+      status: "error",
+      message: "Invalid SectionId!",
+    };
+    res.status(404).json(notFound);
+  } else if (!db._validateContentId(sectionId, contentId)) {
+    const notFound = {
+      status: "error",
+      message: `Invalid Content Id for Section: ${sectionId}!`,
+    };
+    res.status(404).json(notFound);
+  } else {
+    const data = {
+      status: "success",
+      message: `Content: ${contentId} for Section: ${sectionId}, found!`,
+      data: db.contentBySectionIdAndContentId(sectionId, contentId),
+    };
+    res.status(200).json(data);
+  }
+});
 
 app.use(function (req, res) {
   res.status(404).json({
